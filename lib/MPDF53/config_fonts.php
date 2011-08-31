b2b4dc0710401dcd4df99fbd94a79cfad5a4feb0
Huib Keemink <huib.keemink@creativedutchmen.com>
2011-08-31 14:14:57 +0200

    Added liberation font to config

--- lib/MPDF53/config_fonts.php
+++ lib/MPDF53/config_fonts.php
@@ -92,6 +92,12 @@ starting at 1, as they appear in the .ttc file e.g.
 */
 
 $this->fontdata = array(
+	"liberationsans" => array(
+		'R' => "LiberationSans-Regular.ttf",
+		'B' => "LiberationSans-Bold.ttf",
+		'I' => "LiberationSans-Italic.ttf",
+		'BI' => "LiberationSans-Regular-BoldItalic.ttf",
+	),
 	"dejavusanscondensed" => array(
 		'R' => "DejaVuSansCondensed.ttf",
 		'B' => "DejaVuSansCondensed-Bold.ttf",
