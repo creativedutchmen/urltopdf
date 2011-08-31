e9a5e3e451a9692ad9a8d92aaa6522f8724da200
Huib Keemink <huib.keemink@creativedutchmen.com>
2011-08-31 13:36:25 +0200

    Added mpdf in stead of tcpdf. Removed a lot of code which is either not supported in mpdf or not needed. Also removed pdf-attachment page type, because I do not need it (and I couldn't get it to work quickly;))

--- extension.driver.php
+++ extension.driver.php
@@ -12,7 +12,7 @@
 					'website' => 'http://www.bloodbone.ws',
 					'email' => 'brendan@bloodbone.ws'
 				),
-				'description' => 'Uses the TCPDF library to take your HTML page and output it as a PDF'
+				'description' => 'Uses the mPDF library to take your HTML page and output it as a PDF'
 			);
 		}
 
@@ -47,10 +47,6 @@
 					// Page has the 'pdf' type set, so lets generate!
 					$this->generatePDF($context['output']);
 				}
-				else if($type == 'pdf-attachment') {
-					// Page has the 'pdf-attachment' type set, so lets generate some attachments
-					$this->generatePDFAttachments($context['output']);
-				}
 			}
 		}
 
@@ -62,100 +58,17 @@
 			$pdf->SetAuthor($params['website-name']);
 			$pdf->SetTitle($params['page-title']);
 
-			// output the HTML content
-			$pdf->writeHTML($output, true, false, true, false, '');
-
-			// reset pointer to the last page
-			$pdf->lastPage();
+			$pdf->WriteHTML($output);
 
 			//Close and output PDF document
-			$pdf->Output(sprintf('%s - %s', $params['website-name'], $params['page-title']), 'I');
-		}
-
-		public function generatePDFAttachments(&$output) {
-			$params = Frontend::Page()->_param;
-
-			$dom = new DOMDocument('1.0', 'UTF-8');
-			$doc->formatOutput = true;
-			$dom->loadHTML($output);
-
-			if($dom === false) return $output;
-
-			$xpath = new DOMXPath($dom);
-
-			// Copy any <link rel='stylesheet'/> or <style type='text/css'> prepend to the blocks
-			$css = '';
-			$styling = $xpath->query('//link[@rel="stylesheet"] | //style[@type="text/css"]');
-			if($styling->length !== 0) foreach($styling as $style) {
-				$css .= $dom->saveXML($style);
-			}
-
-			// Find anything with @data-utp attribute set to attachment
-			$blocks = $xpath->query('//*[@data-utp = "attachment"]');
-			if($blocks->length !== 0) foreach($blocks as $block) {
-				// Get the content in those blocks
-				$data = $dom->saveXML($block);
-
-				// Send the block to the PDF generator, saving it in /TMP
-				$data = $css . $data;
-				$pdf = self::initPDF();
-
-				// output the HTML content
-				$pdf->writeHTML($data, true, false, true, false, '');
-
-				// reset pointer to the last page
-				$pdf->lastPage();
-
-				// get the output of the PDF as a string and save it to a file
-				// attempt to find the filename if it's provided with @data-utp-filename
-				if(!$filename = $xpath->evaluate('string(//@data-utp-filename)')) {
-					 $filename = md5(sprintf('%s - %s', $params['website-name'], $params['page-title']));
-				}
-				$filename = TMP . '/' . Lang::createFilename($filename) . '.pdf';
-
-				General::writeFile($filename, $pdf->Output($filename, 'S'), Symphony::Configuration()->get('write_mode', 'file'));
-
-				// Replace the attachment node with <link rel='attachment' href='{path/to/file}' />
-				$link = $dom->createElement('link');
-				$link->setAttribute('rel', 'attachment');
-				$link->setAttribute('href', str_replace(DOCROOT, URL, $filename));
-
-				$block->parentNode->replaceChild($link, $block);
-			}
-
-			$output = $dom->saveHTML();
+			$pdf->Output();
+			exit();
 		}
 
 		private static function initPDF() {
-			require_once(EXTENSIONS . '/urltopdf/lib/tcpdf/config/lang/eng.php');
-			require_once(EXTENSIONS . '/urltopdf/lib/tcpdf/tcpdf.php');
+			require_once(EXTENSIONS . '/urltopdf/lib/MPDF53/mpdf.php');
 
-			// create new PDF document
-			$pdf = new TCPDF(PDF_PAGE_ORIENTATION, PDF_UNIT, PDF_PAGE_FORMAT, true, 'UTF-8', false);
-
-			// set document information
-			$pdf->SetCreator(PDF_CREATOR);
-
-			$pdf->setPrintHeader(false);
-			$pdf->setPrintFooter(false);
-
-			// set default monospaced font
-			$pdf->SetDefaultMonospacedFont(PDF_FONT_MONOSPACED);
-
-			//set margins
-			$pdf->SetMargins(20, 20, 20);
-
-			//set auto page breaks
-			$pdf->SetAutoPageBreak(TRUE, 20);
-
-			//set image scale factor
-			$pdf->setImageScale(PDF_IMAGE_SCALE_RATIO);
-
-			//set some language-dependent strings
-			$pdf->setLanguageArray($l);
-
-			// add a page
-			$pdf->AddPage();
+			$pdf = new mpdf();
 
 			return $pdf;
 		}
